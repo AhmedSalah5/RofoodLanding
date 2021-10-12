73c724b305aa8d62a3125754e7067e39a77759f5
AhmedSalah <ahmedmahdy5005@gmail.com>
2021-10-12 20:03:51 +0200

    update js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ $(document).ready(function () {
     //Change Direction
     $('.custum-nav .ar').click(function () {
         $('body').removeClass('ltr')
-        alert("ar")
     })
     $('.custum-nav .en').click(function () {
         $('body').addClass('ltr')
-        alert("En")
     })
 })
